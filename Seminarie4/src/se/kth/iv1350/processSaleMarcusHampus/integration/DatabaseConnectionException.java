0fae72204fdd8fda4b9120af94f938c0c65a6e25
mackan754 <159027316+mackan754@users.noreply.github.com>
2024-05-19 21:01:04 +0200

    Update

--- Seminarie4/src/se/kth/iv1350/processSaleMarcusHampus/integration/DatabaseConnectionException.java
+++ Seminarie4/src/se/kth/iv1350/processSaleMarcusHampus/integration/DatabaseConnectionException.java
@@ -3,7 +3,7 @@ package se.kth.iv1350.processSaleMarcusHampus.integration;
 /**
  * Thrown when a database connection failure occurs.
  */
-public class DatabaseConnectionException extends RuntimeException {
+public class DatabaseConnectionException extends Exception {
     /**
      * Creates a new instance with a message specifying the reason for the database connection failure.
      *
